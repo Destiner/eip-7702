90c7711c35ff25ff389e305313e4f20dd4895d54
Timur Badretdinov <accounts@destiner.io>
2024-06-03 04:27:16 +0400

    Initial commit

---
+++ vocs.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "EIP-7702",
+  theme: {
+    accentColor: {
+      light: "#664400",
+      dark: "#ffab00",
+    },
+  },
+  topNav: [
+    {
+      text: "EIP",
+      link: "https://eips.ethereum.org/EIPS/eip-7702",
+    },
+  ],
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/Destiner/eip-7702",
+    },
+  ],
+  sidebar: [
+    {
+      text: "Overview",
+      link: "/overview",
+    },
+    {
+      text: "Reference",
+      link: "/reference",
+    },
+    {
+      text: "Examples",
+      link: "/examples",
+    },
+    {
+      text: "Ecosystem",
+      link: "/ecosystem",
+    },
+  ],
+});
