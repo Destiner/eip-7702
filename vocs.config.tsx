84a11b1ad6587f8f7a6b552f665d868fb2093f90
Timur Badretdinov <accounts@destiner.io>
2024-11-05 13:45:31 +0000

    Update site description

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -7,7 +7,7 @@ window.metrical = {
 
 export default defineConfig({
   title: "EIP-7702",
-  description: "Set EOA account code for one transaction",
+  description: "Set EOA account code",
   theme: {
     accentColor: "#ffab00",
   },
